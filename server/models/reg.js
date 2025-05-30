16f3df1352942719cf7398d543b9f3b7d638b7c3
Hirendra B <hirendrabalaji3@gmail.com>
2025-05-31 02:21:45 +0530

    gitIgnore

---
+++ server/models/reg.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const registrationSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+}, { timestamps: true });
+
+const Registration = mongoose.model('Registration', registrationSchema);
+export default Registration;
